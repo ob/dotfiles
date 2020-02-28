6eb6ccbd513ed75a483a3fc1f2d6b287016e7b7d
Oscar Bonilla <6f6231@gmail.com>
2020-02-28 10:32:51 -0800

    Add inputrc to dotfiles

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-echo "Nothing to see here yet"
-exit 0
+set -euo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+for dotfile in "$DIR"/dotfiles/*
+do
+    # don't replace it if it's already there
+    bn=$(basename "$dotfile")
+    home_dotfile="$HOME/.$bn"
+    test -f "$home_dotfile" -o -L "$home_dotfile" && continue
+    ln -s "$dotfile" "$home_dotfile"
+done
